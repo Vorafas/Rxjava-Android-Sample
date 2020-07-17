68b7bad4b4dc6f8c204e15c7d0b7506cfae836cc
Сафаров Эльшад Нурали оглы <elshad.safarov@domru.ru>
2020-07-17 14:40:04 +0500

    Оператор distinct

---
+++ rxjavadistinct/src/main/java/com/example/rxjavadistinct/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.rxjavadistinct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Observer;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Observable<Integer> observable = Observable
+                .from(new Integer[]{5, 9, 7, 5, 6, 7, 8, 9, 7, 5})
+                .distinct();
+
+        observable.subscribe(observer);
+    }
+
+    Observer<Integer> observer = new Observer<Integer>() {
+        @Override
+        public void onNext(Integer s) {
+            Log.d(TAG, "onNext: " + s);
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Log.d(TAG, "onError:" + e);
+        }
+
+        @Override
+        public void onCompleted() {
+            Log.d(TAG, "onCompleted");
+        }
+    };
+}
